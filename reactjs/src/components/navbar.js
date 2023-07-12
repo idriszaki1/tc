3f31d63a13a4b5656b696c552797a04eb69794c1
Idris Zaki <idriszaki0618@gmail.com>
2023-07-12 11:50:56 -0400

    Start of new day

--- reactjs/src/components/navbar.js
+++ reactjs/src/components/navbar.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import BLogo from '../resources/logo_black_1.png';
+import {Link} from "react-router-dom";
 
 function navbar() {
   return (
@@ -7,7 +8,13 @@ function navbar() {
       <div className='leftSide'>
         <img src={BLogo} alt='logo'/>
       </div>
-      <div className='rightSide'></div>
+      <div className='rightSide'>
+        <Link to="/">Home</Link>
+        <Link to="/Model-3">Model 3</Link>
+        <Link to="/Model-Y">Model Y</Link>
+        <Link to="/Model-S">Model S</Link>
+        <Link to="/Contact">Contact</Link>
+      </div>
     </div>
   )
 }
