ad0b1809822a0f21fc8764e90ea1ca023f18f9e5
Idris Zaki <idriszaki0618@gmail.com>
2023-08-09 15:22:57 -0400

    setup complete

--- reactjs/src/App.js
+++ reactjs/src/App.js
@@ -1,9 +1,6 @@
 import './App.css';
 import Navbar from "./components/navbar";
 import Home from "./pages/home";
-import Model3 from "./pages/model3";
-import ModelY from "./pages/modely";
-import ModelS from "./pages/models";
 import Contact from "./pages/contact";
 import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
 
@@ -14,9 +11,6 @@ function App() {
         <Navbar />
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/Model-3" element={<Model3 />} />
-          <Route path="/Model-Y" element={<ModelY />} />
-          <Route path="/Model-S" element={<ModelS />} />
           <Route path="/Contact" element={<Contact />} />
         </Routes>
       </Router>
--- reactjs/src/components/navbar.js
+++ reactjs/src/components/navbar.js
@@ -18,17 +18,11 @@ function Navbar() {
         <img src={WLogo} alt='logo'/>
         <div className='hiddenLinks'>
         <Link to="/">Home</Link>
-        <Link to="/Model-3">Model 3</Link>
-        <Link to="/Model-Y">Model Y</Link>
-        <Link to="/Model-S">Model S</Link>
         <Link to="/Contact">Contact</Link>
         </div>
       </div>
       <div className='rightSide'>
         <Link to="/">Home</Link>
-        <Link to="/Model-3">Model 3</Link>
-        <Link to="/Model-Y">Model Y</Link>
-        <Link to="/Model-S">Model S</Link>
         <Link to="/Contact">Contact</Link>
         <button onClick={toggleNavbar}>
           <ReorderIcon />
